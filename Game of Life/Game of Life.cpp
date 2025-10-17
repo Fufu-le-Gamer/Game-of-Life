45a70119f04a57fea3d501ddf8c64d686d2707d6
Fufu-le-Gamer <yducrocq@gaming.tech>
2025-10-17 16:41:21 +0200

    ajout de plusieurs cellules vivantes

--- Game of Life/Game of Life.cpp
+++ Game of Life/Game of Life.cpp
@@ -5,67 +5,60 @@
 #include <chrono>
 #include <conio.h>
 
-
 int main()
 {
 	int SizeX;
 	int SizeY;
 	int X;
 	int Y;
-	int index;
 	int cellule_alive;
-	int* grid = new int[SizeX * SizeY];
 
-	std::cout << "Combien de cellules voulez vous en hauteur ?" << "\n";
-	std::cin >> SizeX;
-	std::cout << "Combien de cellules voulez vous en longueur ?" << "\n";
-	std::cin >> SizeY;
-	std::cout << "Combient de cellules vivantes voulez vous au départ ?" << "\n";
-	std::cin >> cellule_alive;
-	while (cellule_alive > 0) {
-		cellule_alive--;
-		if (cellule_alive > 1) {
-			std::cout << "Quelles sont les coorrdonnees des cellulues vivantes ?" << "\n";
-		}
-		else {
-			std::cout << "Quelles sont les coorrdonnees de la cellulue vivante ?" << "\n";
-		}
-		std::cout << "X: ";
-		std::cin >> X;
-		std::cout << "Y: ";
-		std::cin >> Y;
-	}
-	
-	
-	int* grid = new int[SizeX * SizeY];
-	
+	std::cout << "Combien de cellules voulez-vous en hauteur ?" << "\n";
+    std::cin >> SizeX;
+	std::cout << "Combien de cellules voulez-vous en longueur ?" << "\n";
+    std::cin >> SizeY;
 
-	for (int x = 0; x < SizeX; x++) {
-		for (int y = 0; y < SizeY; y++) {
-			grid[x * SizeY + y] = 0;
-		}
-	}
-	index = X * SizeY + Y;
-	grid[index] = 1;
+    int* grid = new int[SizeX * SizeY];
 
+    for (int i = 0; i < SizeX * SizeY; i++) {
+        grid[i] = 0;
+    }
 
-	for (int x = 0; x < SizeX; x++) {
-		for (int y = 0; y < SizeY; y++) {
-			if (grid[x * SizeY + y] == 0) {
-				std::cout << ". ";
-			}
-			else {
-				std::cout << "[]";
-			}
-			
-		}
-		std::cout << "\n";
-	}
-	
-	delete[] grid;
-	grid = nullptr;
+    std::cout << "Combien de cellules vivantes voulez-vous au depart ?" << "\n";
+    std::cin >> cellule_alive;
 
+    for (int i = 0; i < cellule_alive; i++) {
+        if (cellule_alive > 1) {
+            std::cout << "Coordonnees des cellules vivantes " << i + 1 << " :\n";
+        }
+        else {
+            std::cout << "Coordonnees de la cellule vivante " << i + 1 << " :\n";
+        }
+        std::cout << "X (0 a " << SizeX - 1 << "): ";
+        std::cin >> X;
+        std::cout << "Y (0 a " << SizeY - 1 << "): ";
+        std::cin >> Y;
 
+        if (X >= 0 && X < SizeX && Y >= 0 && Y < SizeY) {
+            grid[X * SizeY + Y] = 1;
+        }
+        else {
+            std::cout << "Coordonnees invalides, cellule ignoree.\n";
+        }
+    }
+    for (int x = 0; x < SizeX; x++) {
+        for (int y = 0; y < SizeY; y++) {
+            if (grid[x * SizeY + y] == 1)
+                std::cout << "[]";
+            else
+                std::cout << ". ";
+        }
+        std::cout << "\n";
+    }
+    for (int y = 0; y < SizeY; y++) {
+        std::cout << "--";
+    }
+    delete[] grid;
+    grid = nullptr;
 }
-
 
